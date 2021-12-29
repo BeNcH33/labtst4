25fb0dc5eaa0e1d272fb292d6336b088d03da46f
BeNcH <slava.morzhakov@vk.com>
2021-12-29 20:32:56 +0300

    цуа

--- TestingLab4/Tests.cs
+++ TestingLab4/Tests.cs
@@ -32,7 +32,6 @@ namespace TestingLab4
 			//Assert.AreEqual(null,VersionsInterval.Union(new VersionsInterval("<1.0.0"),new VersionsInterval(">2.0.0")));
 			Assert.AreEqual( VersionsInterval.Nonunion(new VersionsInterval("<=2.0.0"), new VersionsInterval(">=3.0.0 <=4.0.0")), "0.0.0 2.0.0 || 3.0.0 4.0.0");
 			Assert.AreEqual(VersionsInterval.Nonunion(new VersionsInterval("<=2.0.0"), new VersionsInterval(">=3.0.0")), "0.0.0 2.0.0 || 3.0.0 2147483647.2147483647.2147483647");
-			Assert.AreEqual(VersionsInterval.Union(new VersionsInterval("<2.0.0"), new VersionsInterval(">3.0.0")), null);
 			Assert.AreEqual(VersionsInterval.Union(new VersionsInterval(new Versions("0.0.0"), new Versions("2.0.0")), new VersionsInterval(new Versions("1.0.0"), new Versions("3.0.0"))).ToString(), new VersionsInterval(new Versions("0.0.0"), new Versions("3.0.0")).ToString()); 
 
 		}
